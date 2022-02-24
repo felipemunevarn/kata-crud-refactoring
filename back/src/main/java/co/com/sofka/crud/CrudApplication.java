1e7584ad4bd808a19ca9207ad5f4f2c17b33856e
felipemunevarn <felipemunevarn@gmail.com>
2022-02-23 22:58:50 -0300

    making it runs

--- back/src/main/java/co/com/sofka/crud/CrudApplication.java
+++ back/src/main/java/co/com/sofka/crud/CrudApplication.java
@@ -1,9 +1,14 @@
 package co.com.sofka.crud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan("co.com.sofka.crud.TodoRepository")
 public class CrudApplication {
 
 	public static void main(String[] args) {
